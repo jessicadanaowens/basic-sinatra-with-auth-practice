ba40b2c83275ab8d9ccf97de469d14cd3cfa2d8f
Jessica Owens <jessicadanaowens@gmail.com>
2014-07-06 10:28:43 -0600

    capybara tests

--- app.rb
+++ app.rb
@@ -9,126 +9,56 @@ class App < Sinatra::Application
 
   def initialize
     super
-    @user = []
-    @index = 0
+    @user_database = UserDatabase.new
   end
 
   get "/" do
-    if session[:user_id]
-      @info = find_user_info(session[:user_id])
-      erb :logout, :locals => {:info => @info}
+    if current_user
+      erb :signed_in, locals: {username: current_user[:username]}
     else
-      erb :homepage
+      erb :signed_out
     end
   end
 
-  post "/registration_form" do
-    username = params[:username]
-    password = params[:password]
-    register(username, password)
+  get "/registrations/new" do
+    erb :"registrations/new"
   end
 
-  get "/registration_form" do
-    erb :registration_form
+  post "/registrations" do
+    @user_database.insert(username: params[:username], password: params[:password])
+    flash[:notice] = "Thank you for registering"
+    redirect "/"
   end
 
   post "/sessions" do
-    username = params[:username]
-    password = params[:password]
-    login(username, password)
+    user = find_user(params)
+    session[:user_id] = user[:id] if user
+    redirect "/"
   end
 
-  get "/logout" do
-    erb :logout
-  end
-
-  post "/logout" do
+  get "/signed_out" do
     session.delete(:user_id)
     redirect "/"
   end
 
-  post "/" do
-    erb :homepage
+  get "/signed_in" do
+    erb :signed_in
   end
 
   private
 
-  def check_inputs(username, password, route)
-
-    if username == '' && password == ''
-      flash[:notice]= "Username and Password are required"
-      redirect route
-    elsif username == ''
-      flash[:notice]= "Username is required"
-      redirect route
-    elsif password == ''
-      flash[:notice]= "Password is required"
-      redirect route
-    end
+  def find_user(params)
+    @user_database.all.select { |user|
+      user[:username] == params[:username] && user[:password] == params[:password]
+    }.first
   end
 
-  def login(username, password)
-    check_inputs(username, password, "/")
-    check_registration(username,password)
-    session[:user_id] = user_hash(username).keys[0]
-
-    flash[:notice]= "Welcome"
-    redirect "/"
-  end
-
-  def register(username, password)
-    check_inputs(username, password, "/registration_form")
-    check_if_user_exists(username)
-    enter_user_in_database(username, password)
-
-    flash[:notice]= "Thank you for registering"
-    redirect "/"
-  end
-
-  def check_if_user_exists(username)
-    @user.each do |user_hash|
-      user_hash.each_value do |username_password|
-        if username_password[:user] == username
-          flash[:notice] = "username already taken"
-          redirect '/registration_form'
-        end
-      end
-    end
-  end
-
-  def enter_user_in_database(username, password)
-    id = {index_key => {:user => username, :pass => password}}
-    @user.push(id)
-  end
-
-  def index_key
-    @user.length + 1
-  end
-
-  def user_hash(username)
-    @user.find do |user_hash|
-      user_hash.each_value do |username_password|
-        username_password[:user] == username
-      end
-    end
-  end
-
-  def find_user_info(id)
-    @user.find do |user_hash|
-      user_hash.has_key?(id)
-    end
-  end
-
-  def check_registration(username, password)
-    if user_hash(username) == nil
-      flash[:notice]= "You are not registered"
-      redirect '/registration_form'
-    else
-      if user_hash(username).values[0][:pass] != password
-      flash[:notice]= "Incorrect password. Please re-enter."
-      redirect back
-      end
+  def current_user
+    if session[:user_id]
+      @user_database.find(session[:user_id])
     end
   end
 
 end
+
+
--- spec/capybara_spec.rb
+++ spec/capybara_spec.rb
@@ -5,37 +5,42 @@ require "spec_helper"
       visit "/"
 
       click_on 'Register'
-      expect(page).to have_content("Username:")
-      expect(page).to have_content("Password:")
+      expect(page).to have_content("Username")
+      expect(page).to have_content("Password")
     end
   end
 
-  feature "Hompage" do
-    scenario "user sees flash message when sending a post request to the homepage" do
-      visit "/registration_form"
+  feature "Homepage" do
+    scenario "user can sign in" do
+      visit "/signed_out"
+
+      click_on 'Sign In'
+      expect(page).to have_content("Sign Out")
+    end
+
+  end
+
+  feature "registration form" do
+    scenario "user can fill out form and create an account" do
+      visit "/registrations/new"
 
       click_on 'Register'
-      expect(page).to have_content("Thank you")
+      expect(page).to have_content("Thank you for registering")
     end
   end
 
-feature "Hompage" do
-  scenario "User can Login and see flash message" do
-    visit "/"
+  feature "signed in page" do
+    scenario "user can click logout and be logged out" do
+      visit "/signed_in"
 
-    click_on 'Login'
-    expect(page).to have_content("Welcome")
+      click_on 'Sign Out'
+      expect(page).to have_content("Sign In")
+    end
+
   end
-end
 
-feature "Click Log out" do
-  scenario "User clicks logout and is redirected to homepage" do
-    visit "/logout"
 
-    click_on 'Logout'
-    expect(page).to have_content("Username:")
-  end
-end
+
 
 
 
