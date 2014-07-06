0a3c225e387bcc256f996feaeb14aef084d5557c
Jessica Owens <jessicadanaowens@gmail.com>
2014-07-05 21:36:39 -0600

    advanced part of the exercise

--- app.rb
+++ app.rb
@@ -9,17 +9,23 @@ class App < Sinatra::Application
 
   def initialize
     super
-    @user_database = UserDatabase.new
+    @user = []
+    @index = 0
   end
 
   get "/" do
-    "Hello, world"
-    erb :homepage
+    if session[:user_id]
+      @info = find_user_info(session[:user_id])
+      erb :logout, :locals => {:info => @info}
+    else
+      erb :homepage
+    end
   end
 
   post "/registration_form" do
-    flash[:notice]= "Thank you for registering"
-    redirect "/"
+    username = params[:username]
+    password = params[:password]
+    register(username, password)
   end
 
   get "/registration_form" do
@@ -27,16 +33,9 @@ class App < Sinatra::Application
   end
 
   post "/sessions" do
-    #insert the session object as the user and insert the username and password into @users
-    #@users is an array of hashes where the session points to a hash that contains the attributes
-
-    cookie = (session[:user_id] = 1)
     username = params[:username]
     password = params[:password]
-    # @user_database.insert(cookie)
-    # @user_database.validate!(cookie, {username => params[:username], password => params[:password]})
-    flash[:notice]= "Welcome #{username}"
-    redirect "/logout"
+    login(username, password)
   end
 
   get "/logout" do
@@ -52,5 +51,84 @@ class App < Sinatra::Application
     erb :homepage
   end
 
+  private
+
+  def check_inputs(username, password, route)
+
+    if username == '' && password == ''
+      flash[:notice]= "Username and Password are required"
+      redirect route
+    elsif username == ''
+      flash[:notice]= "Username is required"
+      redirect route
+    elsif password == ''
+      flash[:notice]= "Password is required"
+      redirect route
+    end
+  end
+
+  def login(username, password)
+    check_inputs(username, password, "/")
+    check_registration(username,password)
+    session[:user_id] = user_hash(username).keys[0]
+
+    flash[:notice]= "Welcome"
+    redirect "/"
+  end
+
+  def register(username, password)
+    check_inputs(username, password, "/registration_form")
+    check_if_user_exists(username)
+    enter_user_in_database(username, password)
+
+    flash[:notice]= "Thank you for registering"
+    redirect "/"
+  end
+
+  def check_if_user_exists(username)
+    @user.each do |user_hash|
+      user_hash.each_value do |username_password|
+        if username_password[:user] == username
+          flash[:notice] = "username already taken"
+          redirect '/registration_form'
+        end
+      end
+    end
+  end
+
+  def enter_user_in_database(username, password)
+    id = {index_key => {:user => username, :pass => password}}
+    @user.push(id)
+  end
+
+  def index_key
+    @user.length + 1
+  end
+
+  def user_hash(username)
+    @user.find do |user_hash|
+      user_hash.each_value do |username_password|
+        username_password[:user] == username
+      end
+    end
+  end
+
+  def find_user_info(id)
+    @user.find do |user_hash|
+      user_hash.has_key?(id)
+    end
+  end
+
+  def check_registration(username, password)
+    if user_hash(username) == nil
+      flash[:notice]= "You are not registered"
+      redirect '/registration_form'
+    else
+      if user_hash(username).values[0][:pass] != password
+      flash[:notice]= "Incorrect password. Please re-enter."
+      redirect back
+      end
+    end
+  end
 
 end
